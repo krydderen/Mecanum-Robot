7307d22dd8c7efe8ee2837b2bca8e1cf4e992a5e
Kevin Moen <kevinms@stud.ntnu.no>
2020-09-23 13:28:22 +0200

    Reverting changes.

--- Python/Comms/client.py
+++ Python/Comms/client.py
@@ -1,5 +1,15 @@
 import socket
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = socket.gethostbyname('www.google.com')
-print(ip)
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(b'Hello')
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print("received data:", data)
