7fee27d2196e89ecd93cab927a7f46464aa88ecf
Kevin Moen <kevmoesto@gmail.com>
2020-09-30 14:40:10 +0200

    Kivy testing.
    Got a searchbox up and running which can take inputs

--- Python/GUI/KIVY/Testing/WeatherApp.py
+++ Python/GUI/KIVY/Testing/WeatherApp.py
@@ -1,8 +1,17 @@
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
+class AddLocationForm(BoxLayout):
+    search_input = ObjectProperty()
+        
+    def search_location(self):
+            print("The user searched for {}".format(self.search_input.text))
 
 class WeatherApp(App):
+    title = "WeatherApp"
     pass
-
 
 if __name__ == '__main__':
     WeatherApp().run()
