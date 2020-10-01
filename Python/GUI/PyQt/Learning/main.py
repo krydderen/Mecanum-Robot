0f9632035f4f31f4525f515a8e5b664b0892254a
Kevin Moen <kevmoesto@gmail.com>
2020-10-01 10:08:40 +0200

    PyQt5 testing.
    Bleep bloop toolbar goes poop

--- Python/GUI/PyQt/Learning/main.py
+++ Python/GUI/PyQt/Learning/main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QAction, QApplication, QLabel, QPushButton,QMainWindow, QStatusBar, QToolBar
+from PyQt5.QtWidgets import QAction, QApplication, QCheckBox, QLabel, QPushButton,QMainWindow, QStatusBar, QToolBar
 from PyQt5 import QtWidgets
 
 
@@ -26,17 +26,38 @@ class MainWindow(QMainWindow):
         button_action.setCheckable(True)
         toolbar.addAction(button_action)
         
+        toolbar.addSeparator()
+        
+        button_action2 = QAction(QIcon("./Python/resources/icons/icons/bug.png"), "Your button2", self)
+        button_action2.setStatusTip("This is your second button")
+        button_action2.triggered.connect(self.onMyToolBarButtonClick)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+        
+        toolbar.addSeparator()
+        
+        toolbar.addWidget(QLabel("Hello"))
+        
+        toolbar.addSeparator()
+        
+        toolbar.addWidget(QCheckBox())
+        
         self.setStatusBar(QStatusBar(self))
 
     def onMyToolBarButtonClick(self, s):
         print("Click", s)
 
-    
-
-
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
 app = QApplication(sys.argv)
 
 window = MainWindow()
-window.show()
+window.show() # IMPORTANT!!!!! Windows are hidden by default.
 
+# Start the event loop.
 app.exec_()
+
+
+# Your application won't reach here until you exit and the event 
+# loop has stopped.
