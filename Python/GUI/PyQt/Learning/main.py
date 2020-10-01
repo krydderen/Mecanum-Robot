837799db8f3b75e1183dad3c6d14a32b42de78c2
Kevin Moen <kevmoesto@gmail.com>
2020-10-01 09:52:32 +0200

    PyQt5 testing.
    Added icons for guis hehe

--- Python/GUI/PyQt/Learning/main.py
+++ Python/GUI/PyQt/Learning/main.py
@@ -1,26 +1,37 @@
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QWidget,QMainWindow
+from PyQt5.QtWidgets import QAction, QApplication, QLabel, QPushButton,QMainWindow, QStatusBar, QToolBar
+from PyQt5 import QtWidgets
 
-class CustomButton(QPushButton):
-    
-    def keyPressEvent(self, event):
-        super(CustomButton, self).keyPressEvent(event)
-    
 
 class MainWindow(QMainWindow):
-    
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         
-        self.setWindowTitle("Heia Vilde")
+        self.setWindowTitle("My vilde App")
         
-        label = QLabel("Hei igjen Vilde")
-        
+        label = QLabel("THIS IS Vilde!!!")
         label.setAlignment(Qt.AlignCenter)
-        
+
         self.setCentralWidget(label)
+        
+        toolbar = QToolBar("My Vilde Toolbar")
+        self.addToolBar(toolbar)
+
+        button_action = QAction("Your button", self)
+        button_action.setStatusTip("This is your button")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True)
+        toolbar.addAction(button_action)
+        
+        self.setStatusBar(QStatusBar(self))
+
+    def onMyToolBarButtonClick(self, s):
+        print("Click", s)
+
     
+
 
 app = QApplication(sys.argv)
 
