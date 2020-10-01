b854ea60b41d09b4e529f074255676921c9a655a
Kevin Moen <kevmoesto@gmail.com>
2020-10-01 09:21:04 +0200

    PyQt5 testing.
    Set up a window and proceeding with the button

--- Python/GUI/PyQt/Learning/main.py
+++ Python/GUI/PyQt/Learning/main.py
@@ -1,12 +1,30 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QWidget,QMainWindow
+
+class CustomButton(QPushButton):
+    
+    def keyPressEvent(self, event):
+        super(CustomButton, self).keyPressEvent(event)
+    
+
+class MainWindow(QMainWindow):
+    
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        
+        self.setWindowTitle("Heia Vilde")
+        
+        label = QLabel("Hei igjen Vilde")
+        
+        label.setAlignment(Qt.AlignCenter)
+        
+        self.setCentralWidget(label)
+    
 
 app = QApplication(sys.argv)
 
-
-window = QWidget()
-window.setWindowTitle("Fy faen kom i røva mit")
+window = MainWindow()
 window.show()
-
 
 app.exec_()
