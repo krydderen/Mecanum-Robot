aad6a0fb98fdaad4a8821b7ec1734060b8d9ccb2
Kevin Moen <kevmoesto@gmail.com>
2020-10-02 13:59:02 +0200

    PyQt5 & OpenCV testing.
    It is pretty disgusting but it does work right now. Will not refer to how many times I have avoided errors and disabled them because it is absolute garbage. If this happens again hopefully trump passes away.

--- Python/GUI/PyQt/Learning/maincameratest.py
+++ Python/GUI/PyQt/Learning/maincameratest.py
@@ -1,8 +1,8 @@
-import cv2
 import sys
 from PyQt5.QtWidgets import  QWidget, QLabel, QApplication
 from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QImage, QPixmap
+import cv2
 
 class Thread(QThread):
     changePixmap = pyqtSignal(QImage)
@@ -24,7 +24,11 @@ class Thread(QThread):
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        [...]
+        self.title = 'PyQt5 Video'
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 480
         self.initUI()
 
     @pyqtSlot(QImage)
@@ -34,12 +38,17 @@ class App(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.resize(1800, 1200)
+        #self.resize()
         # create a label
         self.label = QLabel(self)
-        self.label.move(280, 120)
+        #self.label.move(280, 120)
         self.label.resize(640, 480)
         th = Thread(self)
         th.changePixmap.connect(self.setImage)
         th.start()
         self.show()
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
