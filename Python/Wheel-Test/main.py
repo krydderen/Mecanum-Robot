3a22ca34b6d1effe25ac4e10a8d765fb6f9ec78f
Kevin Moen <kevmoesto@gmail.com>
2020-10-19 15:24:09 +0200

    Testing diags.

--- Python/Wheel-Test/main.py
+++ Python/Wheel-Test/main.py
@@ -26,24 +26,24 @@ while run:
     
     keys = pygame.key.get_pressed()
     
-    diagonals = False
+    move = False
     
     
     if keys[pygame.K_w] and keys[pygame.K_d]:
         print('wd')
-        diagonals = True; y -= vel; x += vel
+        move = True; y -= vel; x += vel
         motor_controller.diagonal(drivetime = drive_time, inputspeed = drive_speed)
     elif keys[pygame.K_w] and keys[pygame.K_a]:
         print('wa')
-        diagonals = True; y -= vel; x -= vel
+        move = True; y -= vel; x -= vel
         motor_controller.diagonal(drivetime = drive_time, inputspeed = drive_speed)
     elif keys[pygame.K_s] and keys[pygame.K_d]:
         print('sd')
-        diagonals = True; y += vel; x = vel
+        move = True; y += vel; x = vel
         motor_controller.diagonal(drivetime = drive_time, inputspeed = drive_speed)
     elif keys[pygame.K_s] and keys[pygame.K_a]:
         print('sa')
-        diagonals = True; y += vel; x -= vel
+        move = True; y += vel; x -= vel
         motor_controller.diagonal(drivetime = drive_time, inputspeed = drive_speed)
     
     # Change motor speed to high or low.
@@ -56,34 +56,42 @@ while run:
             print('Drive speed is now LOW')       
         
         
-    if event.type == pygame.KEYDOWN and diagonals != True:
+    if event.type == pygame.KEYDOWN and move != True:
         if event.key == pygame.K_UP or event.key == ord('w'):
             print('up')
+            move = True
             y -= vel
             motor_controller.forward(drivetime = drive_time, inputspeed = drive_speed)
         elif event.key == pygame.K_DOWN or event.key == ord('s'):
             print('down')
+            move = True
             y += vel
             motor_controller.backward(drivetime = drive_time, inputspeed = drive_speed)
         elif event.key == pygame.K_LEFT or event.key == ord('a'):
             print('left')
+            move = True
             x -= vel
             motor_controller.left(drivetime = drive_time, inputspeed = drive_speed)
         elif event.key == pygame.K_RIGHT or event.key == ord('d'):
             print('right')
+            move = True
             x += vel
             motor_controller.right(drivetime = drive_time, inputspeed = drive_speed)
         elif event.key == ord('q'):
             print('counterclockwise')
+            move = True
             motor_controller.rotate(direction = 'COUNTER_CLOCKWISE',drivetime = drive_time, inputspeed = drive_speed)
         elif event.key == ord('e'):
             print('clockwise')
+            move = True
             motor_controller.rotate(direction = 'CLOCKWISE',drivetime = drive_time, inputspeed = drive_speed)
-    elif diagonals != True:
-        pass
-    else:
+    
+    if move == False:
         motor_controller.stop()
+    else:
+        pass
         
+    
     win.fill((0,0,0))  # Fills the screen with black
     pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
     pygame.display.update() 
